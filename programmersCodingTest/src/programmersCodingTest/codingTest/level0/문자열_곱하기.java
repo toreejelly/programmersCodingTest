e69bc2d3701e712936f92e07bca766356844eaa5
toreejelly <toreejelly@gmail.com>
2023-06-09 23:14:18 +0900

    문자열 곱하기

---
+++ programmersCodingTest/src/programmersCodingTest/codingTest/level0/문자열_곱하기.java
@@ -0,0 +1,43 @@
+package programmersCodingTest.codingTest.level0;
+
+
+public class 문자열_곱하기 {
+
+	public String solution(String my_string, int k) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<k; i++){
+            sb.append(my_string);
+            answer = sb.toString();
+        }//for end
+     
+        return answer;
+        
+	}//
+	
+	public static void main(String[] args) {
+			/*
+			 
+			 * 
+			 
+			 * 문제실행 : 
+			 * 제한사항 : 
+						
+			 * 입력 : 
+			 * 출력 : 
+			 
+			 * 결론 : 
+			 	1.  
+			 		
+				2. 
+			 	
+			 */
+		
+		   
+		
+	        
+	    }//end
+	}//class end
+
+
