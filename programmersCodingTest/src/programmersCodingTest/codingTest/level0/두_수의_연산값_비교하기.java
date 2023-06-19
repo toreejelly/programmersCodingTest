e53a9b72736eaf38c16774276adddb48de9ca163
toreejelly <toreejelly@gmail.com>
2023-06-19 23:45:43 +0900

    두 수의 연산값 비교하기

---
+++ programmersCodingTest/src/programmersCodingTest/codingTest/level0/두_수의_연산값_비교하기.java
@@ -0,0 +1,53 @@
+package programmersCodingTest.codingTest.level0;
+
+
+public class 두_수의_연산값_비교하기 {
+ 
+	    public int solution(int a, int b) {
+	        
+	        int AB = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+	        int ab = 2*a*b;
+	        int answer = 0;
+	        
+	        answer = (AB>ab)? AB:ab ;
+	        
+	        return answer;      
+	        
+	       
+	    }//end
+
+	public static void main(String[] args) {
+			/*
+			 
+			 * 
+			 
+			 * 문제실행 : 
+			   연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+
+				12 ⊕ 3 = 123
+			    3 ⊕ 12 = 312
+			   양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
+			   
+			   단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다. 
+			 
+
+						
+			 * 입출력 : 
+ 				a	b	result
+				2	91	364
+				91	2	912
+			 
+			 * 결론 : 
+			 	1. 단은 어케 하는건가?
+			 		
+				2. 
+			 	
+			 */
+		
+		   
+		
+	        
+	    }
+	}//end
+
+
