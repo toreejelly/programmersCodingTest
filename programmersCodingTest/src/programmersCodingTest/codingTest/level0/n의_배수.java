f6c1d0db56cbbf7d64b8c9e2499a47b2ddfdfcd8
toreejelly <toreejelly@gmail.com>
2023-06-19 23:45:57 +0900

    n의 배수

---
+++ programmersCodingTest/src/programmersCodingTest/codingTest/level0/n의_배수.java
@@ -0,0 +1,48 @@
+package programmersCodingTest.codingTest.level0;
+
+
+
+public class n의_배수 {
+	public int solution(int num, int n) {
+        int answer = 0;
+        
+        
+        //x의 n의 배수 확인하기 
+        //x % n == 0
+        
+        answer = (num % n == 0) ? 1 : 0;
+        
+        return answer;
+	}
+
+	public static void main(String[] args) {
+			/*
+			 
+			 * 
+			 
+			 * 문제실행 : 
+			    		정수 num과 n이 매개 변수로 주어질 때, 
+			  			num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+			  
+						
+			 * 입출력 : 
+						num	n	result
+						98	2	1
+						34	3	0
+			 
+			 * 결론 : 
+			 	1. x의 n의 배수 확인하기 
+        		   x % n == 0 
+			 		
+				2. 
+			 	
+			 */
+		
+		   
+		
+	        
+	    }
+	}
+
+
+
