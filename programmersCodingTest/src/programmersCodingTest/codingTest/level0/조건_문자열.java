43f0ca12de9240e0f90746bf307953ea2d23a636
toreejelly <toreejelly@gmail.com>
2023-06-19 23:45:29 +0900

    조건 문자열

---
+++ programmersCodingTest/src/programmersCodingTest/codingTest/level0/조건_문자열.java
@@ -0,0 +1,34 @@
+package programmersCodingTest.codingTest.level0;
+
+
+
+public class 조건_문자열 {
+
+	
+	public static void main(String[] args) {
+			/*
+			 
+			 * 문제실행 : 
+			 				
+			 * 입출력 :  
+			 
+			 * 결론 : 
+			 	1.  
+			 		
+				2. 
+			 	
+			 */
+		
+		String ineq = (">");
+		String eq = ("=");
+		String sum = (">" + "=");
+		int n = 7;
+		int m = 9;
+		
+		System.out.println(n + sum + m);
+		
+	        
+	    }
+	}
+
+
