0bbc9c6ff20820be56e743a17d09484aaab9f5f5
toreejelly <toreejelly@gmail.com>
2023-06-20 23:36:35 +0900

    주사위 게임2

---
+++ programmersCodingTest/src/programmersCodingTest/codingTest/level0/주사위_게임_2.java
@@ -0,0 +1,35 @@
+package programmersCodingTest.codingTest.level0;
+
+
+
+public class 주사위_게임_2 {
+
+	public static void main(String[] args) {
+			/*
+			 
+			 * 문제실행 : 
+			 				
+			 * 입출력 :  
+			 
+			 * 결론 : 
+			 	1. 제곱
+			 	   Math.pow() 함수는 매개변수와 반환값이 모두 double 형식입니다. 
+			 	   따라서 정수 값을 거듭제곱하고 싶을 때에는 반환값을 다시 정수로 변환해야 합니다.
+			 	   int result = (int) Math.pow(a, b);
+				
+				   안에서 덧셈은 불가능
+				   (int)(Math.pow(a, 2 + b,2)) -> x
+				   (int)(Math.pow(a, 2)) + (int)(Math.pow(b, 2))  -> o
+				   
+			 		
+				2. 
+			 	
+			 */
+		
+		   
+		
+	        
+	    }
+	}
+
+
